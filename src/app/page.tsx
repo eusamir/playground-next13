d20aa56f987aa459cc66aaa4229e74c6e9656624
Samir <samir.andrade@ceuma.br>
2023-07-17 18:29:39 -0300

    estilizando card de item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,34 @@
 import Image from 'next/image'
 import camisa1 from '../assets/camisa1.png'
+import { HomeContainer,Product } from '@/styles/pages/home'
 
 export default function Home() {
   return (
-    <main>
-      <a>
+    <HomeContainer>
+      <Product>
         <Image src={camisa1} width={520} height={480} alt={'camisa1'} />
 
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
-      </a>
-      <a>
+      </Product>
+      <Product>
         <Image src={camisa1} width={520} height={480} alt={'camisa1'} />
 
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
-      </a>
-    </main>
+      </Product>
+      <Product>
+        <Image src={camisa1} width={520} height={480} alt={'camisa1'} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
